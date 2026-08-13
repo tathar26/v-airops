6cd0ee6fc4ec648cbe17181ba13396afd37b8598
Glenn Satory <glenn.satory@tricore.be>
2026-08-13 10:50:16 +0200

    feat: add scheduler and queue worker services to docker configurations and define scheduled console task

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+use Illuminate\Support\Facades\Schedule;
+
+// Run the global data aggregation daily at midnight
+Schedule::command('system:aggregate-airline-data')->daily();
