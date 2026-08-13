fa6aa18baa517729a6982cd768af8f674352e790
Glenn Satory <glenn.satory@tricore.be>
2026-08-14 00:42:18 +0200

    fix: set SCRIPT_FILENAME, SCRIPT_NAME, and PHP_SELF in bridge files to force Symfony Request pathInfo resolution

--- public/dispatch.php
+++ public/dispatch.php
@@ -1,4 +1,7 @@
 <?php
 
 $_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
 require_once __DIR__ . '/index.php';
--- public/fsacars/dispatch.php
+++ public/fsacars/dispatch.php
@@ -1,10 +1,12 @@
 <?php
 
-$_SERVER['SCRIPT_NAME'] = '/index.php';
-
 $indexPath = __DIR__;
 while (!file_exists($indexPath . '/index.php') && dirname($indexPath) !== $indexPath) {
     $indexPath = dirname($indexPath);
 }
+
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = $indexPath . '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
 
 require_once $indexPath . '/index.php';
