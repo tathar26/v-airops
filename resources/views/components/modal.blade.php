d39aae3da697d20ec7efd2247edd796748f50f2f
Glenn Satory <glenn.satory@tricore.be>
2026-08-13 14:51:26 +0200

    fix: add 3xl-7xl modal sizes and safe array fallback to components/modal.blade.php

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -3,13 +3,20 @@
 @php
 $id = $id ?? md5($attributes->wire('model'));
 
-$maxWidth = [
+$maxWidthMap = [
     'sm' => 'sm:max-w-sm',
     'md' => 'sm:max-w-md',
     'lg' => 'sm:max-w-lg',
     'xl' => 'sm:max-w-xl',
     '2xl' => 'sm:max-w-2xl',
-][$maxWidth ?? '2xl'];
+    '3xl' => 'sm:max-w-3xl',
+    '4xl' => 'sm:max-w-4xl',
+    '5xl' => 'sm:max-w-5xl',
+    '6xl' => 'sm:max-w-6xl',
+    '7xl' => 'sm:max-w-7xl',
+];
+
+$maxWidth = $maxWidthMap[$maxWidth ?? '2xl'] ?? 'sm:max-w-2xl';
 @endphp
 
 <div
